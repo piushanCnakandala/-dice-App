f5a8a49bbbd467512bc7dd6c49375221d7893ce5
piushanCnakandala <piushancnakandala2000@gmail.com>
2023-06-02 13:09:11 +0530

    app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+              child: Text('Dicee')),
           backgroundColor: Colors.blue,
         ),
         body: DicePage(),
@@ -15,7 +18,24 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber=1;
+  int rightDiceNumber=6;
+
+  void changeDiceFace(){
+    setState(() {
+      leftDiceNumber= Random().nextInt(6) +1; //now print 1-6
+      rightDiceNumber= Random().nextInt(6) +1; //now print 1-6
+    });
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,9 +44,39 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: TextButton(
               onPressed: (){
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),),),
+
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),),),
+        ],
+      ),
+    );
+  }}
+
+
+/*
+class DicePage extends StatelessWidget {
+  int leftDiceNumber=5;
+  @override
+  Widget build(BuildContext context) {
+
+    leftDiceNumber=2;
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: (){
                 print('Left button got pressed.');
               },
-              child: Image.asset('images/dice1.png'),),),
+              child: Image.asset('images/dice$leftDiceNumber.png'),),),
           Expanded(
             child: TextButton(
               onPressed: (){
@@ -37,4 +87,4 @@ class DicePage extends StatelessWidget {
       ),
     );
   }
-}
+}*/
